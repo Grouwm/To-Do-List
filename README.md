89a27d7d6b9db6fab23b2770cc57506accdd5402
Danial <danialbarbodjalali@gmail.com>
2024-10-12 23:21:11 +0330

    Add Everything
    Everything In its right place

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# To-Do-List
+# Work-Note
+To remember and manage your work, save them in the form of a note!!
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./assets/html/index.html'],
+  theme: {
+    fontFamily: {
+      'fedraarabic_title': ['fedraarabic_title'],
+      'portadaARA_text': ['portadaARA_text'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

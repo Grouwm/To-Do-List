3ea4d04638bea49e4cf0e7701c88c66d7d44c087
Danial <danialbarbodjalali@gmail.com>
2024-10-13 17:30:56 +0330

    Update functions/note
    Had to change the code a bit due to the changes in index.html

--- js/note.js
+++ js/note.js
@@ -1,15 +1,41 @@
-const checkbox = document.getElementById("myCheckbox");
-const labels = document.querySelectorAll(".label");
-
-checkbox.addEventListener("change", () => {
-  labels.forEach((label) => {
-    label.classList.toggle("line-through", checkbox.checked);
-  });
-});
+// importing the functions file
 import { addNote, loadNotes } from "./functions.js";
-document.addEventListener("DOMContentLoaded", loadNotes);
-const addNoteButton = document.querySelector("button");
+
+const redColor = document.querySelector("#red");
+const greenColor = document.querySelector("#green");
+const yellowColor = document.querySelector("#yellow");
+
+redColor.addEventListener("click", () => {
+  toggleSelectedColor("red");
+});
+
+greenColor.addEventListener("click", () => {
+  toggleSelectedColor("green");
+});
+
+yellowColor.addEventListener("click", () => {
+  toggleSelectedColor("yellow");
+});
+
+function toggleSelectedColor(color) {
+  redColor.classList.remove("selected");
+  greenColor.classList.remove("selected");
+  yellowColor.classList.remove("selected");
+
+  if (color === "red") {
+    redColor.classList.add("selected");
+  } else if (color === "green") {
+    greenColor.classList.add("selected");
+  } else if (color === "yellow") {
+    yellowColor.classList.add("selected");
+  }
+}
+// save btn eventlistener
+const addNoteButton = document.querySelector("#save-btn");
 addNoteButton.addEventListener("click", (event) => {
   event.preventDefault();
   addNote();
 });
+
+// Loading the notes when page is loading
+document.addEventListener("DOMContentLoaded", loadNotes);
